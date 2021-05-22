c298682724cf94228676666f6f9ead9070d0a2ac
gonzaggb <gonzaggb@hotmail.com>
2021-05-22 16:56:37 -0300

    agrego public path en mainCcontroller

--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -1,3 +1,7 @@
+const path = require('path')
+const app = express()
+publicPath = path.join(__dirname,'../public')
+app.use(express.static(publicPath));
 /*.render es un metodo de EJS que indica a la aplicación que debe mostrar la pagina, similar al sendFile de express*/
 const controller = {
     index: (req, res) => {
