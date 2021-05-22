4424d7659d4716ab1952f89666f8498241bf3da5
gonzaggb <gonzaggb@hotmail.com>
2021-05-22 16:53:23 -0300

    agregado public en routes

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ publicPath = path.join(__dirname,'../public') // indica que la ruta contiene rec
 console.log(publicPath);
 
 app.use(express.static(publicPath)); //indica que los archivos son publicos
-app.listen(port,()=>console.log('Servidor corriendo en '+port))
+app.listen(port,()=>console.log('Servidor corriendo en '+ port))
 /*HAY QUE INSTALAR npm install ejs --save para poder utilizar el template engine*/
 app.set('view engine', 'ejs') // configuro el template engine
 app.use('/', mainRoute) // defino a donde ir en caso de ingresar "/", voy al siguiente archivo const mainRoute = require('./routes/main')
--- src/routes/main.js
+++ src/routes/main.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const mainController = require('../controllers/mainController')
+const path = require('path')
+const app = express()
+publicPath = path.join(__dirname,'../public')
+app.use(express.static(publicPath));
 
 router.get('/', mainController.index) // en caso de recibir solo la barra voy a la propiedar index del controlador main
 router.get('/login', mainController.login) // en caso de recibir solo la barra voy a la propiedar login del controlador main
